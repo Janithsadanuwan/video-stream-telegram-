b80b162ebd01620fe96c5b54a7c1f7aeeeba768d
Janith sadanuwan/ <95123183+Janithsadanuwan@users.noreply.github.com>
2021-12-29 13:52:06 -1200

    Update userbotjoin.py

--- program/userbotjoin.py
+++ program/userbotjoin.py
@@ -1,5 +1,5 @@
 import asyncio
-from driver.js import user
+from driver.janith import user
 from pyrogram.types import Message
 from pyrogram import Client, filters
 from config import BOT_USERNAME, SUDO_USERS
