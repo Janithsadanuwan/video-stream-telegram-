a1828fd5db73ee80821713f8686960171fd57ee2
Janith sadanuwan/ <95123183+Janithsadanuwan@users.noreply.github.com>
2021-12-29 13:49:06 -1200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import asyncio
 from pytgcalls import idle
-from driver.js import call_py, bot
+from driver.janith import call_py, bot
 
 async def start_bot():
     print("[INFO]: STARTING BOT CLIENT")
